c7297cc5bc21546183dddaa2d60f040ccb4e5753
kzi-arch <zakiaddaroni9@gmail.com>
2026-05-23 04:52:17 +0700

    model update

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     GEMINI_API_KEY: str
-    GEMINI_MODEL: str = "gemini-3.5-flash"
+    GEMINI_MODEL: str = "gemini-3.1-flash-lite"
 
 # Optimasi Token & Cost
     TEMPERATURE: float = 0.7
